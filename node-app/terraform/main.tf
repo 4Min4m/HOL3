6f43bae0d33a9163d6534c87d05fe41b7639cb7f
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-15 15:00:35 +0000

    initializing files

---
+++ node-app/terraform/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name = "node-app"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "node-app"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "node-app"
+        }
+      }
+      spec {
+        container {
+          image = "ghcr.io/4min4m/node-app:latest"
+          name  = "node-app"
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = "node-app-service"
+  }
+  spec {
+    selector = {
+      app = "node-app"
+    }
+    port {
+      port        = 80
+      target_port = 3000
+    }
+    type = "NodePort"
+  }
+}
